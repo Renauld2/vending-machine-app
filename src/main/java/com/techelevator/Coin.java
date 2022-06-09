b4e6e4c066644b3d8af0eb45b7319b31478f2f8e
Nicholas Soilis <nicholassoilis@gmail.com>
2022-06-09 13:32:55 -0400

    Added Coin and Money Classes

---
+++ src/main/java/com/techelevator/Coin.java
@@ -0,0 +1,11 @@
+package com.techelevator;
+
+public class Coin {
+    private String name;
+    private double worth;
+
+    public Coin(String name, double worth){
+        this.name = name;
+        this.worth = worth;
+    }
+}
---
+++ src/main/java/com/techelevator/Money.java
@@ -0,0 +1,10 @@
+package com.techelevator;
+
+public class Money {
+
+    public Money(){
+        Coin quarter = new Coin("Quarter", 0.25);
+        Coin dime = new Coin("Dime", 0.10);
+        Coin nickle = new Coin("Nickle", 0.05);
+    }
+}
